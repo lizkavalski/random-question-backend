d1de8f918acf60a1f07dc3f3dd406aa594af292b
lizkavalski <lizkavalski@gmail.com>
2020-07-12 11:31:13 -0700

    updated the file

--- server/middleware/helperRandom.js
+++ server/middleware/helperRandom.js
@@ -1,36 +1,15 @@
 const questions = require('../data/data.json');
 /**
-* Pulls a random object from the data.json
+* Pulls a random object from the data.json.
 */
 const randomQuestions = () => {
   return questions[Math.floor(Math.random() * questions.length)];
 };
-
 /**
- * Get N random question from a questionArray
+ * Pulls a random object based on 'type' from the data.json.
  */
-const randomN = (questionArray, n) => {
-  const limit = questionArray.length < n ? questionArray.length : n;
-  const randomIndicesSet = new Set();
-
-  while (randomIndicesSet.size < limit) {
-    const randomIndex = Math.floor(Math.random() * questionArray.length);
-    if (!randomIndicesSet.has(randomIndex)) {
-      randomIndicesSet.add(randomIndex);
-    }
-  }
-
-  return Array.from(randomIndicesSet).map(randomIndex => {
-    return questionArray[randomIndex];
-  });
+const questionByType = (type) => {
+  let filterType = questions.filter(question => question.type === type);
+  return filterType[Math.floor(Math.random() * filterType.length)];
 };
-
-/**
- * @param {*} type, refers to the type of question
- * @param {*} n, how many random question
- */
-const questionByType = (type, n) => {
-  return randomN(questions.filter(question => question.type === type), n);
-};
-
-module.exports = { questions, randomQuestions, questionByType };
+module.exports = { questions, randomQuestions, questionByType};
